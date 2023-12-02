272dcf74797ca68bffeaa31a1c90c76025eaa4e6
Maks <m.posiel@gmail.com>
2023-12-02 21:21:07 +0100

    Added interface, improved functionality

--- script.js
+++ script.js
@@ -1,26 +1,83 @@
 const myLibrary = [];
 
 function Book(title, author, pagesCount, read) {
-    this.title = title;
-    this.author = author;
-    this.pagesCount = pagesCount;
-    this.read = read;
+  this.title = title;
+  this.author = author;
+  this.pagesCount = pagesCount;
+  this.read = read;
 }
-
 
 function addBookToLibrary(title, author, pagesCount, read) {
-    myLibrary.push(new Book(title, author, pagesCount, read));
-
+  myLibrary.push(new Book(title, author, pagesCount, read));
 }
+const popup = document.querySelector(".add-book-popup");
 
+const addBookBtn = document.querySelector(".add-new-btn");
+addBookBtn.addEventListener("click", () => {
+    popup.style.visibility = "visible";
+});
+
+const closeBtn = document.querySelector(".close-btn");
+closeBtn.addEventListener("click", () => {
+    popup.style.visibility = "hidden";
+});
+
+const submitBtn = document.querySelector(".submit-btn");
+
+submitBtn.addEventListener('click', () => {
+    const title = document.querySelector("#title");
+    const author = document.querySelector("#author");
+    const pages = document.querySelector("#pages");
+    const read = document.querySelector("#read");
+
+    addBookToLibrary(title.value, author.value,pages.value,read.checked);
+    popup.style.visibility = "hidden";
+    displayLibrary();
+})
 
 function displayLibrary() {
+  const booksSection = document.querySelector(".books");
+
   for (let i = 0; i < myLibrary.length; i++) {
-    console.log(
-      `Title: ${myLibrary[i].title} Author: ${myLibrary[i].author} Number of pages: ${myLibrary[0].pagesCount} Already read: ${myLibrary[0].read}`
-    );
+    const newDiv = document.createElement("div");
+    const titleDiv = document.createElement("div");
+    const authorDiv = document.createElement("div");
+    const pagesDiv = document.createElement("div");
+    const readDiv = document.createElement("div");
+    const readButton = document.createElement("button");
+    readButton.classList.add("read-button");
+    readButton.innerHTML = "Read";
+    readButton.addEventListener("click", () => {
+      if (myLibrary[i].read === false) {
+        myLibrary[i].read = true;
+      } else {
+        myLibrary[i].read = false;
+      }
+      readDiv.innerHTML = `Read: ${myLibrary[i].read}`;
+    });
+
+    titleDiv.innerHTML = `Title: ${myLibrary[i].title}`;
+    authorDiv.innerHTML = `Author: ${myLibrary[i].author}`;
+    pagesDiv.innerHTML = `Pages: ${myLibrary[i].pagesCount}`;
+    readDiv.innerHTML = `Read: ${myLibrary[i].read}`;
+
+    newDiv.appendChild(titleDiv);
+    newDiv.appendChild(authorDiv);
+    newDiv.appendChild(pagesDiv);
+    newDiv.appendChild(readDiv);
+
+    newDiv.appendChild(readButton);
+
+    newDiv.id = myLibrary[i].title;
+    newDiv.classList.add("book");
+    if (document.querySelector(`#${myLibrary[i].title}`) === null) {
+      booksSection.appendChild(newDiv);
+    }
   }
 }
 
+addBookToLibrary("Harry", "Rowling", 566, true);
+addBookToLibrary("HarryP", "asRowling", 56564, false);
+addBookToLibrary("HarrsdfgsdfgyP", "asRowling", 56564, false);
 
 displayLibrary();
